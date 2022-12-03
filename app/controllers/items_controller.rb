016dfb5a5132d545e2a3c3e7db25ef8d6bf26147
martin mithi <gathitumithi9000@gmail.com>
2022-12-03 18:16:01 +0300

    commit tree 3d297583419d976b49b602d6b4e7d655a6a253e0

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,24 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_response_not_found
+
 
   def index
+    if params[:user_id]
+      user=User.find(params[:user_id])
+      items=user.items
+    else
     items = Item.all
+    end
     render json: items, include: :user
   end
 
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def render_response_not_found
+    render json: {error: "User not found"}, status: :not_found
+  end
 end
